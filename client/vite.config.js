e708261968ad0fc01ec8eccf070a4af22ecf2bcc
vadim.cvirko <promiti90@gmail.com>
2025-10-28 12:04:49 +0300

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../server/dist', // билд пойдёт прямо в express
-    emptyOutDir: true,
-  },
-});
+	plugins: [react()],
+	build: {
+		outDir: '../server/dist', // билд пойдёт прямо в express
+		emptyOutDir: true,
+	},
+})
