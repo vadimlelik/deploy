720a69b234e75f96fc7e5979cc812046c0fb713f
vadim.cvirko <promiti90@gmail.com>
2025-10-28 11:57:05 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser'
 import { postRouter } from './route/post.router.js'
 import { authRouter } from './route/auth.router.js'
 import { fileURLToPath } from 'url'
+import path from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
